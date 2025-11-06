215f4787379918bbbe0990038929538948b837d5
SaikoDiablo <vikkiti37@gmail.com>
2025-11-06 14:43:08 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int size =10;
+        int[][] matrix = new int [size][size];
+
+        for(int i=0; i<size;i++){
+          matrix[i][i]=i;
+       }
+        for (int i = 0; i<size; i++){
+            for (int j = 0; j<size; j++){
+              System.out.print(matrix[i][j] + " ");
+            }
+         System.out.println();   
+        }
+
+        int sum =0;
+        for(int i=0; i<10;i++)
+          sum+=matrix[i][i];
+        System.out.println("Sum of diagonal:"+sum);  
+    }
+}    
