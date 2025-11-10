224c53d94f8d2a619039e16d8ad8c1731b5c7443
SaikoDiablo <vikkiti37@gmail.com>
2025-11-10 13:37:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int size =10; // here we defining the size of a matrix fro not using the nested loops too much and make code cleaner
-        int[][] matrix = new int [size][size];// creating a 10on10 arary
-
-        for(int i=0; i<size;i++){ // using for loop in order to fill the diagonal with numbers
-          matrix[i][i]=i;
-       }
-       // using for to print the matrix
-        for (int i = 0; i<size; i++){// this loop is for each row
-            for (int j = 0; j<size; j++){// this for each column
-              System.out.print(matrix[i][j] + " ");//print matrix with space
+        int size =10; 
+        int[][] matrix = new int [size][size];
+       
+        for (int i = 0; i<size; i++){
+            for (int j = 0; j<size; j++){
+              matrix[i][i]=i;
+              System.out.print(matrix[i][j] + " ");
             }
-         System.out.println();  // move text to the next line for matrix to look like one 
+         System.out.println();  
         }
 
-        int sum =0;// sum variable
-        for(int i=0; i<10;i++)//calculate the sum
+        int sum =0;
+          for(int i=0; i<10;i++)
           sum+=matrix[i][i];
-        System.out.println("Sum of diagonal:"+sum);  // print the sum
+        System.out.println("Sum of diagonal:"+sum);  
     }
 }    
